ec6adf06338a2ead330b450a620fa93ee97a33fc
wngai01 <mongaiwaiyin@gmail.com>
2024-09-07 18:57:18 -0700

    Updates

--- world/main.js
+++ world/main.js
@@ -2,20 +2,13 @@
 // interface setup
 const carCanvas=document.getElementById("carCanvas");
 carCanvas.width=930;
-// const networkCanvas=document.getElementById("networkCanvas");
-// networkCanvas.width=300;
-// const miniMapCanvas=document.getElementById("miniMapCanvas");
-// miniMapCanvas.width=300;
-// miniMapCanvas.height=300;
 
 carCanvas.height=window.innerHeight;
-// networkCanvas.height = window.innerHeight - 300;
 
 const carCtx = carCanvas.getContext("2d");
-// const networkCtx = networkCanvas.getContext("2d");
 
 const viewport = new Viewport(carCanvas, world.zoom, world.offset);
-// const miniMap = new MiniMap(miniMapCanvas, world.graph, 300);
+
 
 //training amount
 const maxSpeed = 3;
@@ -24,37 +17,53 @@ const geneticValue = 1;
 const cars = generateCars(N);
 let bestCar = cars[0];
 
-// genetic function
-if(localStorage.getItem("bestBrain")){
-    for(let i = 0; i < cars.length; i++) {
-        cars[i].brain = JSON.parse(
-            localStorage.getItem("bestBrain")
-        );
-        if(i != 0) {
+const hardcodedBrainData = {
+    "levels": [
+        {
+            "inputs": [0.4506697086777014, 0, 0, 0.3559188589086042, 0.548074461389779],
+            "outputs": [0.6352044807526437, 0, 0, 0, 0, 0.632857725394856],
+            "biases": [-0.556917874987583, -0.6731662474453071, 0.52411910267265, -0.18877510500240136, 0.7752749335763516, -0.9084112510572545],
+            "weights": [
+                [-0.21440645354277577, -0.9663046644762321, -0.5967737168374999, -0.8816463832380259, 0.27906255478847863, -0.6621235204972967],
+                [-0.3816712254654289, -0.5166529741371507, 0.17260513335254385, 0.5035122771294908, -0.43360857361389593, -0.14053759231896168],
+                [0.8096746987337259, 0.37401498192288496, -0.08768668587179129, 0.4878237232700157, -0.3224616105884889, 0.6798417676257934],
+                [-0.3317155590006221, -0.34686283065344203, -0.07799254865562111, -0.07900090663776377, 0.23152099842212637, 0.9153874500153323],
+                [0.5345567867194868, -0.32501493028536593, -0.19056903640616543, 0.27706276395588225, 0.2867262571596449, -0.5527682634066111]
+            ]
+        },
+        {
+            "inputs": [0.6352044807526437, 0, 0, 0, 0, 0.632857725394856],
+            "outputs": [0.5368483943350216, 0, 0, 0],
+            "biases": [0.13266543134514752, 0.9324772119919065, 0.5469407653482601, 0.12942916035384625],
+            "weights": [
+                [0.1268013580901144, 0.5746997570835117, -0.5898830402629268, 0.05722491744750691],
+                [0.4453051570037812, 0.8632410226106823, -0.4783111958314304, 0.84772825885549],
+                [-0.3475719870157459, -0.009850805299186333, -0.6972995746776673, 0.6891049061818361],
+                [0.7434653216284559, 0.7508933756170391, -0.8512910526343821, 0.940534442289851],
+                [0.42010767217860717, -0.5079892802697329, 0.5761899868662278, 0.30866007633130343],
+                [0.930649988492019, 0.8872276779537501, -0.7681357973082492, -0.6145220218007046]
+            ]
+        }
+    ]
+};
+
+if (hardcodedBrainData) {
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].brain = hardcodedBrainData;
+        if (i != 0) {
             // lower the value the more similar the cars' brain to other new gen AI
             // recommend initial = 10, fine tune = 0.05
-
-            NerualNetwork.mutate(cars[i].brain, geneticValue); 
+            NerualNetwork.mutate(cars[i].brain, geneticValue);
         }
     }
 }
+
 
 // initializaing the road borders and traffic
 const traffic = [];
 const roadBorders = world.roadBorders.map((s) => [s.p1, s.p2]);
 
 animate();
-
-// saving the NN of the best car
-function save() {
-    localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
-}
-
-// delete the NN of the best car
-function discard() {
-    localStorage.removeItem("bestBrain");
-}
-
 function generateCars(N) {
     const startPoints = world.markings.filter((m) => m instanceof Start);
     const cars = [];
